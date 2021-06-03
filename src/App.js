d05771d6e9cce091ea9538e1de106ac775da9d34
sanajitjana <sanajitjana1402@gmail.com>
2021-06-04 03:12:01 +0530

    update title and use setinterval

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const time = new Date().toLocaleTimeString();
+  const [count, setCount] = useState(time);
+
+  const updateTime = () => {
+    const currTime = new Date().toLocaleTimeString();
+    setCount(currTime);
+  };
+
+  setInterval(updateTime, 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-center mt-5">React - Digital Clock</h1>
+      <div className="text-center" style={{ marginTop: "30vh" }}>
+        <h1>{count}</h1>
+      </div>
+      <h6 className="text-center" style={{ marginTop: "40vh" }}>
+        Copyright &copy; 2021 Sanajit Jana. All Rights Reserved.
+      </h6>
+    </>
   );
-}
+};
 
 export default App;
